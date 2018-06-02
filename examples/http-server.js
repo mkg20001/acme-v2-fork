5340dd1e4b010e4d425ca8033883ea43d97670a2
Maciej Krüger <mkg20001@gmail.com>
2018-06-02 13:01:13 +0200

    feat: JS standard codestyle

--- examples/http-server.js
+++ examples/http-server.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
-var http = require('http');
-var express = require('express');
+var http = require('http')
+var express = require('express')
 var server = http.createServer(express.static('../tests')).listen(80, function () {
-  console.log('Listening on', this.address());
-});
+  console.log('Listening on', this.address())
+})
