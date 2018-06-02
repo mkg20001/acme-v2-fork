3568bee3fbe655fdc0a48c33952c0b164fbb2cd2
Maciej Krüger <mkg20001@gmail.com>
2018-06-02 13:01:13 +0200

    feat: JS standard codestyle

--- examples/https-server.js
+++ examples/https-server.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-var https = require('https');
+var https = require('https')
 var server = https.createServer({
-  key: require('fs').readFileSync('../tests/privkey.pem')
-, cert: require('fs').readFileSync('../tests/fullchain.pem')
+  key: require('fs').readFileSync('../tests/privkey.pem'),
+  cert: require('fs').readFileSync('../tests/fullchain.pem')
 }, function (req, res) {
-  res.end("Hello, World!");
+  res.end('Hello, World!')
 }).listen(443, function () {
-  console.log('Listening on', this.address());
-});
+  console.log('Listening on', this.address())
+})
